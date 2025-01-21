684f64069801f04e1e1121dafc572f3b49bcf80c
abrar997 <abraralrawi997.com>
2025-01-21 23:44:26 +0300

    check search to all pages

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -7,9 +7,13 @@ import Footer from "../components/reusable/Footer";
 import { useContext } from "react";
 import { AuthContext } from "../context/authContext";
 import { AccountLinks } from "../assets/LinksPageData";
+import FilteredProducts from "../components/reusable/FilteredProducts";
+import { ProductsContext } from "../context/context";
 
 const Account = () => {
   const { user, userGoogleData } = useContext(AuthContext);
+  const { filteredProducts, searchProduct } = useContext(ProductsContext);
+
   let itemIndex = 0;
 
   const inputsData = [
@@ -30,88 +34,94 @@ const Account = () => {
     <div>
       <TopHeader />
       <Header isCheckout />
-      <div className="lg:mt-[80px] mt-8 lg:mb-[140px] mb-12 lg:p-inline p-4 grid lg:gap-[80px] gap-10">
-        <div className="flex justify-between items-center font-secondary">
-          <PathPages links={AccountLinks} lastIndex={1} />
-          <div className="flex gap-2 text-sm">
-            <span>Welcome!</span>
-            <span className="text-secondary capitalize">
-              {user ? (
-                user.userName
-              ) : userGoogleData ? (
-                userGoogleData.name
-              ) : (
-                <p>user name</p>
-              )}
-            </span>
-          </div>
-        </div>
-        <div className="grid lg:grid-cols-3 lg:gap-0 gap-4">
-          <div className="capitalize col-span-1 flex flex-col lg:gap-6 gap-3 font-secondary lg:text-md">
-            <div className="grid gap-4">
-              <h1 className="font-medium">manage my account</h1>
-              <div className="lg:pl-9 pl-2 grid gap-2">
-                <Link to="/account" className="text-secondary">
-                  my Profile
-                </Link>
-                <Link to="" className="opacity-50">
-                  Address Book
-                </Link>
-                <Link to="" className="opacity-50">
-                  My Payment Options
-                </Link>
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <h1 className="font-medium">manage my account</h1>
-              <div className="lg:pl-9 pl-2 grid gap-2">
-                <Link to="" className="opacity-50">
-                  my returns
-                </Link>
-                <Link to="" className="opacity-50">
-                  my cancellations
-                </Link>
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <h1 className="font-medium">my wishlist</h1>
+      {searchProduct?.trim().length > 0 && filteredProducts?.length > 0 ? (
+        <FilteredProducts />
+      ) : (
+        <div className="lg:mt-[80px] mt-8 lg:mb-[140px] mb-12 lg:p-inline p-4 grid lg:gap-[80px] gap-10">
+          <div className="flex justify-between items-center font-secondary">
+            <PathPages links={AccountLinks} lastIndex={1} />
+            <div className="flex gap-2 text-sm">
+              <span>Welcome!</span>
+              <span className="text-secondary capitalize">
+                {user ? (
+                  user.userName
+                ) : userGoogleData ? (
+                  userGoogleData.name
+                ) : (
+                  <p>user name</p>
+                )}
+              </span>
             </div>
           </div>
+          <div className="grid lg:grid-cols-3 lg:gap-0 gap-4">
+            <div className="capitalize col-span-1 flex flex-col lg:gap-6 gap-3 font-secondary lg:text-md">
+              <div className="grid gap-4">
+                <h1 className="font-medium">manage my account</h1>
+                <div className="lg:pl-9 pl-2 grid gap-2">
+                  <Link to="/account" className="text-secondary">
+                    my Profile
+                  </Link>
+                  <Link to="" className="opacity-50">
+                    Address Book
+                  </Link>
+                  <Link to="" className="opacity-50">
+                    My Payment Options
+                  </Link>
+                </div>
+              </div>
+              <div className="grid gap-4">
+                <h1 className="font-medium">manage my account</h1>
+                <div className="lg:pl-9 pl-2 grid gap-2">
+                  <Link to="" className="opacity-50">
+                    my returns
+                  </Link>
+                  <Link to="" className="opacity-50">
+                    my cancellations
+                  </Link>
+                </div>
+              </div>
+              <div className="grid gap-4">
+                <h1 className="font-medium">my wishlist</h1>
+              </div>
+            </div>
 
-          <div className="lg:shadow-contactBox grid gap-6 lg:py-10 py-6 lg:px-20 px-0 lg:col-span-2">
-            <div className="grid gap-4">
-              <h1 className="text-md font-secondary text-secondary font-medium">
-                Edit your profile
-              </h1>
-              <form
-                action=""
-                className={`grid ${
-                  itemIndex > 2 ? "grid-cols-1" : "lg:grid-cols-2"
-                } lg:gap-6 gap-5`}
-              >
-                {inputsData.map((input, index) => {
-                  const isLstThree = index >= inputsData.length - 3;
-                  return (
-                    <InputField
-                      key={index}
-                      label={input.label}
-                      placeholder={input.placeholder}
-                      type={input.type}
-                      className={`${isLstThree ? "col-span-full w-full" : ""}`}
-                    />
-                  );
-                })}
-              </form>
-            </div>
-            <div className="ml-auto flex items-center gap-8">
-              <button className="border-none bg-transparent text-md">
-                Cancel
-              </button>
-              <Button text="save changes" />
+            <div className="lg:shadow-contactBox grid gap-6 lg:py-10 py-6 lg:px-20 px-0 lg:col-span-2">
+              <div className="grid gap-4">
+                <h1 className="text-md font-secondary text-secondary font-medium">
+                  Edit your profile
+                </h1>
+                <form
+                  action=""
+                  className={`grid ${
+                    itemIndex > 2 ? "grid-cols-1" : "lg:grid-cols-2"
+                  } lg:gap-6 gap-5`}
+                >
+                  {inputsData.map((input, index) => {
+                    const isLstThree = index >= inputsData.length - 3;
+                    return (
+                      <InputField
+                        key={index}
+                        label={input.label}
+                        placeholder={input.placeholder}
+                        type={input.type}
+                        className={`${
+                          isLstThree ? "col-span-full w-full" : ""
+                        }`}
+                      />
+                    );
+                  })}
+                </form>
+              </div>
+              <div className="ml-auto flex items-center gap-8">
+                <button className="border-none bg-transparent text-md">
+                  Cancel
+                </button>
+                <Button text="save changes" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
       <Footer />
     </div>
   );
